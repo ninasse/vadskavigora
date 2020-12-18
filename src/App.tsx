5ffd3fde26909fe73b2d3f53f2e89c879a8ba100
ninasse <58419590+ninasse@users.noreply.github.com>
2020-12-18 12:24:16 +0100

    added and installed routing and created Home and Admin components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Admin from './components/admin/Admin';
+import Home from './components/home/Home';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to vadskavigoranu.se!</h1>
+   
+    <Router>
+        <header className="navbar">
+          <nav className="nav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>             
+              </li>            
+              <li>
+                <Link to="/admin">Admin</Link>             
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/home">
+              <Home />           
+            </Route>
+            <Route path="/admin">
+              <Admin />           
+            </Route>
+            <Route exact path="/">
+              <Home />           
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/components/admin/Admin.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Admin.scss';
+
+export default function Admin(){
+    return (
+        <h1>Admin works!</h1>
+    )
+}
