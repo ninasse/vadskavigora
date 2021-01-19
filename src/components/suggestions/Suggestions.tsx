40d012e6e49be145a5b23d5c84c856f2ce03af7e
ninasse <58419590+ninasse@users.noreply.github.com>
2021-01-19 23:06:55 +0100

    setting states and styling in Suggestions comp.

--- src/components/suggestions/Suggestions.tsx
+++ src/components/suggestions/Suggestions.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, MouseEvent} from 'react';
-import firebase from '../../firebase';
+import {database} from '../../firebase';
 import Suggestion from './../../models/Suggestion';
 import './Suggestions.scss';
 
@@ -10,38 +10,42 @@ export default function Suggestions(props: ISuggestionsProps){
 
     const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
     const [isClicked, setIsClicked] = useState(false);
-    const suggestionsRef = firebase.database().ref('Suggestions');
+    const [selectedSuggestion, setSelectedSuggestion] = useState<HTMLLIElement>();
     const [expandDiv, setExpandDiv] = useState(Boolean);
 
-   useEffect(() => {
+    const suggestionsRef = database.ref('Suggestions');
+    useEffect(() => {
     
-    suggestionsRef.on('value', (snapshot)=> {
-        const suggestions = snapshot.val();
-        const suggestionsArr: Suggestion[] = []
-        for(let i in suggestions){
-            suggestionsArr.push(suggestions[i]);
-        }
-        setSuggestions(suggestionsArr)  
+        suggestionsRef.on('value', (snapshot)=> {
+            const suggestions = snapshot.val();
+            const suggestionsArr: Suggestion[] = []
+            for(let i in suggestions){
+                suggestionsArr.push(suggestions[i]);
+            }
+            setSuggestions(suggestionsArr)  
         })
   // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []); 
 
-    
+    function deleteSuggestion(){
+        //console.log(suggestions)
+       // suggestionsRef.child()
+        
+    }
     function liClicked(e: MouseEvent<HTMLElement>) {
         e.preventDefault();
-       console.log(e);
-        
-       const otherLis = document.querySelector('.expandLi');
+        console.log(e);
 
-       otherLis?.setAttribute('class','suggestionList');
+       if(selectedSuggestion?.className === 'expandLi'){
+           selectedSuggestion.className = 'suggestionList'
+       };
 
-       let value = (e.target as HTMLLIElement).value;
-       let selectedLi = e.currentTarget as HTMLLIElement;
+        let selectedLi = e.currentTarget as HTMLLIElement;
+        selectedLi.className='expandLi';
 
-       selectedLi.value === value ? selectedLi.className='expandLi' : selectedLi.className='suggestionList';
-
+        setSelectedSuggestion(selectedLi)
         setIsClicked(true);
-       setExpandDiv(!expandDiv);
+        setExpandDiv(!expandDiv);
     }
 
     if (isClicked){
@@ -55,19 +59,13 @@ export default function Suggestions(props: ISuggestionsProps){
         <ul>
             {suggestions.map((suggestion : any) =>  {
                return <li className="suggestionList" onClick={liClicked} key=
-               {suggestion.ID}>
-                   
-                    <h3>{suggestion.title}
-                   </h3>
-                    <p>{suggestion.description}
-                    </p>
-                  <p> länk: 
-                {suggestion.link} 
-                  </p>
-
-                  <button /* onClick="deleteSuggestion" */ className="deleteButton" >
+               {suggestion.ID}>                   
+                    <h3>{suggestion.title}</h3>
+                    <p>{suggestion.description}</p>
+                    <p> länk: {suggestion.link}</p>
+                    <button id={suggestion.ID}  onClick={deleteSuggestion}  className="deleteButton" >
                            Radera
-                       </button>
+                    </button>
                 </li>
             })}
         </ul>
