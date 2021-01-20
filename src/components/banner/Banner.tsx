54a405064701aa31341eda4192b28b1a56f65d5f
Caroline Svanemar <caroline.svanemar@hotmail.com>
2021-01-20 23:19:43 +0100

    =adding banner component

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.scss';
+
+
+export default function Banner(){
+    return(
+        <React.Fragment>
+            <div id="bannerContainer">
+                <h3>Hjälp oss hjälpa!</h3>
+                <p>
+                    Hjälp oss att fylla på aktivitetsbanken med massor av roliga aktiviteter som andra föräldrar kan ta del av. I en tid av vabb och brist på ider är det kanske just din aktivitet som ger glädje åt andra barn.
+                </p>
+            </div>
+ 
+        </React.Fragment>
+        
+       
+    )
+}
